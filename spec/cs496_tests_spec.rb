de0c955cad0574c6c1e98249de17de5389c264ab
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-09-27 01:03:47 -0700

    clean up

--- spec/cs496_tests_spec.rb
+++ spec/cs496_tests_spec.rb
@@ -2,82 +2,68 @@ require_relative 'spec_helper'
 
 context 'cs496 tests' do
 
+  before :all do
+    @my_user = GistWrapper.user
+  end
+
+  before :each do
+    @gist_content = {
+      description: 'the description for this gist',
+      public: true,
+      files: {
+        gist_name: {
+          content: Time.now.to_s
+        }
+      }
+    }
+  end
+
   it '1. Getting public Gists returns 30 Gists' do
     public_gists = GistWrapper.public_gists
     expect(public_gists.length).to eq 30
   end
 
   it '2. The user `wolfordj` has at least one public Gist' do
-    my_user = GistWrapper.user username: 'wolfordj'
-    expect(my_user.gist?).to eq true
+    wolfordj = GistWrapper.user username: 'wolfordj'
+    expect(wolfordj.gist?).to eq true
   end
 
   it '3. When you create a Gist the number of Gists associated to your
       account increases by 1' do
-    my_user = GistWrapper.user
-    before = my_user.gists.length
-    content = {
-      description: 'the description for this gist',
-      public: true,
-      files: {
-        Time.now.to_s => {
-          content: 'String file contents'
-        }
-      }
-    }
+    before = @my_user.gists.length
     begin
-      response = my_user.create_gist(content)
-      after = my_user.gists.length
+      response = @my_user.create_gist(@gist_content)
+      after = @my_user.gists.length
     ensure
-      my_user.delete_gist(id: response[:id])
+      @my_user.delete_gist(id: response[:id])
     end
     expect(after).to eq(before + 1)
   end
 
   it '4. The contents of the Gist you created match the contents you send' do
-    my_user = GistWrapper.user
-    expected = {
-      description: 'the description for this gist',
-      public: true,
-      files: {
-        gist_name: {
-          content: Time.now.to_s
-        }
-      }
-    }
     begin
-      id = my_user.create_gist(expected)[:id]
-      gists = my_user.gists
-      content = gists.each { |e| break e.content if e.id == id }
+      id = @my_user.create_gist(@gist_content)[:id]
+      content = @my_user.gist(id: id).content
     ensure
-      my_user.delete_gist(id: id)
+      @my_user.delete_gist(id: id)
     end
-    expect(content).to eq expected[:files][:gist_name][:content]
+    expect(content).to eq @gist_content[:files][:gist_name][:content]
   end
 
   it '5. You are able to edit the contents of a Gist' do
-    my_user = GistWrapper.user
-    expected = {
-      description: 'the description for this gist',
-      public: true,
-      files: {
-        gist_name: {
-          content: Time.now.to_s
-        }
-      }
-    }
     begin
-      id = my_user.create_gist(expected)[:id]
-      my_user.edit_gist(id: id,
-                        files: { gist_name: { content: 'updated content' } })
-      content = my_user.gist(id: id).content
+      id = @my_user.create_gist(@gist_content)[:id]
+      @my_user.edit_gist(id: id,
+                         files: { gist_name: { content: 'updated content' } })
+      content = @my_user.gist(id: id).content
     ensure
-      my_user.delete_gist(id: id)
+      @my_user.delete_gist(id: id)
     end
     expect(content).to eq 'updated content'
   end
 
   it '6. You can add a star to a Gist' do
+    
 
   end
 
@@ -90,23 +76,13 @@ context 'cs496 tests' do
   end
 
   it '9. You can delete a Gist' do
-    my_user = GistWrapper.user
-    content = {
-      description: 'the description for this gist',
-      public: true,
-      files: {
-        Time.now.to_s => {
-          content: 'String file contents'
-        }
-      }
-    }
     begin
-      gist_id = my_user.create_gist(content)[:id]
+      id = @my_user.create_gist(@gist_content)[:id]
     ensure
-      my_user.delete_gist(id: gist_id)
+      @my_user.delete_gist(id: id)
     end
-    my_gist_ids = my_user.gists.map { |gist| gist[:id] }
-    expect(my_gist_ids.include?(gist_id)).to eq false
+    my_gist_ids = @my_user.gists.map { |gist| gist[:id] }
+    expect(my_gist_ids.include?(id)).to eq false
   end
 
 end
